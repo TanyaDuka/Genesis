8865494cdd246471d6e3445bb767bcfa3a07d610
TanyaDuka <duka.tanik@gmail.com>
2023-03-19 14:54:59 +0200

    fix0

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {  Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import CoursesPage from "./components/CoursesPage";
 import LessonPage from "./components/LessonPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    
       <Routes>
         <Route path="/courses" element={<CoursesPage />} />
         <Route path={"/courses/:id"} element={<LessonPage />} />
         <Route path="*" element={<Navigate to="/courses" />} />
       </Routes>
-    </BrowserRouter>
+  
   );
 }
 
